e24d31a3836e2a233bf491539458b34aec929b54
Developers@Work <DevelopersWork@users.noreply.github.com>
2022-02-26 01:22:54 +0530

    Update Bot.py

--- ViewBot/Bot.py
+++ ViewBot/Bot.py
@@ -16,7 +16,7 @@ import traceback
 from urllib.parse import urlparse, parse_qs
 import string
 
-TIMEOUT = (3, 15)
+TIMEOUT = (10, 30)
 RETRIES = 5
 YT_HEAD_URL = """http://s.youtube.com/api/stats/watchtime?ns=yt&el=detailpage&cpn=isWmmj2C9Y2vULKF&docid={0}&ver=2&cmt=7334&ei={1}&fmt=133&fs=0&rt=1003&of={2}&euri&lact=4418&live=dvr&cl={3}&state=playing&vm={4}&volume={5}&c=MWEB&cver=2.20200313.03.00&cplayer=UNIPLAYER&cbrand=apple&cbr=Safari%20Mobile&cbrver=12.1.15E148&cmodel=iphone&cos=iPhone&cosver=12_2&cplatform=MOBILE&delay=5&hl=ru&cr=GB&rtn=1303&afmt=140&lio=1556394045.182&idpj=&ldpj=&rti=1003&muted=0&st=7334&et=7634"""
 
