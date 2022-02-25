d3596a2abbfd53e49ac5e6c26da0d20f2fc49140
Developers@Work <DevelopersWork@users.noreply.github.com>
2022-02-25 11:04:17 +0530

    Update Manager.py

--- ViewBot/Manager.py
+++ ViewBot/Manager.py
@@ -87,7 +87,7 @@ class Manager:
         _active_workers = self.__values["active"] 
 
         __result = self.__values["watching"] < self.__values["threads"]
-        __result = __result and (_passive_workers < _to_run) and (_active_workers > (_passive_workers//4))
+        __result = __result and (_passive_workers < _to_run) and ((_active_workers >= (_passive_workers//4) or _passive_workers == 0 or _active_workers == 0))
 
         self.__queue[p_id].pop(0)
         self.__critical[p_id] = False
